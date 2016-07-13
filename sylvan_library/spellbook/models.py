744a0969ba6e39e34227716773b16bd5f69b3bb4
marshl <marshl@live.com.au>
2016-07-13 23:00:57 +1000

    Removed the Type and Subtype models;

--- sylvan_library/spellbook/models.py
+++ sylvan_library/spellbook/models.py
@@ -3,14 +3,6 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from django.utils.regex_helper import Choice
 
-# Create your models here.
-
-class Type(models.Model):
-    name = models.CharField(max_length = 50, unique = True)
-    
-class Subtype(models.Model):
-    name = models.CharField(max_length = 50, unique = True)
-
 class Block(models.Model):
     name = models.CharField(max_length = 200, unique = True)
     release_date = models.DateField()
