a2574d5c510a20055c8475062cc6fdde9f3ffc17
marshl <marshl@live.com.au>
2016-08-07 22:54:21 +1000

    Added a basic set and card detail page;

--- sylvan_library/spellbook/urls.py
+++ sylvan_library/spellbook/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'spellbook'
 urlpatterns = [
    url(r'^$', views.index, name='index'),
+   
+   url(r'^card/(?P<card_id>[0-9]+)/$', views.card_detail, name='card_detail'),
+   url(r'^set/(?P<set_code>[a-zA-Z0-9]+)/$', views.set_detail, name='set_detail'),
 ]
