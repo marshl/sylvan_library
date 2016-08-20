d9e12ebcf49689d9c409553e6692c11546d51e9b
marshl <marshl@live.com.au>
2016-08-20 23:42:11 +1000

    Added Block data updates;

--- sylvan_library/spellbook/management/commands/_paths.py
+++ sylvan_library/spellbook/management/commands/_paths.py
@@ -1,7 +1,9 @@
 from os import path
 
 json_zip_download_url = "http://mtgjson.com/json/AllSets-x.json.zip"
-output_data_folder = path.abspath('spellbook/data')
-json_zip_path = path.join(output_data_folder, 'AllSets-x.json.zip')
-json_data_path = path.join(output_data_folder, 'AllSets-x.json')
-pretty_json_path = path.join(output_data_folder, 'AllSets-x-pretty.json')
+data_folder = path.abspath('spellbook/data')
+json_zip_path = path.join(data_folder, 'AllSets-x.json.zip')
+json_data_path = path.join(data_folder, 'AllSets-x.json')
+pretty_json_path = path.join(data_folder, 'AllSets-x-pretty.json')
+
+language_json_path = path.join(data_folder, 'languages.json')
--- sylvan_library/spellbook/management/commands/update_database.py
+++ sylvan_library/spellbook/management/commands/update_database.py
@@ -1,9 +1,12 @@
 from django.core.management.base import BaseCommand
+import json
+from os import path
+
 from ...models import Card, CardPrinting, CardPrintingLanguage, PhysicalCard
 from ...models import PhysicalCardLink, UserOwnedCard, UserCardChange, DeckCard
 from ...models import Deck, CardTagLink, CardTag, CardRuling, Rarity, Block
 from ...models import Set, Language
-from . import _query, _parse
+from . import _query, _parse, _rarity, _paths
 
 class Command(BaseCommand):
     help = 'Downloads the MtG JSON data file'
@@ -14,7 +17,6 @@ class Command(BaseCommand):
         json_data = sorted(json_data.items(),
                        key=lambda card_set: card_set[1]["releaseDate"])
 
-
         self.update_rarity_table()
         self.update_language_information()
         self.update_block_information(json_data)
@@ -23,24 +25,67 @@ class Command(BaseCommand):
         self.update_ruling_table(json_data)
         self.update_physical_cards(json_data)
 
-
     def update_rarity_table(self):
-        pass
+        rarity_list = _rarity.rarity_list()
+
+        for r in rarity_list:
+
+            obj = None
+            try:
+                obj = Rarity.objects.get(symbol=r.sym)
+                obj.name = r.name
+                obj.display_order = r.display_order
+
+            except Rarity.DoesNotExist:
+                obj = Rarity(symbol=r.sym, name=r.name, display_order=r.display_order)
+
+            obj.save()
+
 
     def update_language_information(self):
+
+        # print(path.abspath('languages.json'))
+        f = open(_paths.language_json_path, 'r', encoding="utf8")
+        languages = json.load(f, encoding='UTF-8')
+        f.close()
+
+        for lang in languages:
+
+            obj = None
+            try:
+                obj = Language.objects.get(name=lang['name'])
+                obj.mci_code = lang['code']
+
+            except Language.DoesNotExist:
+                obj = Language(name=lang['name'], mci_code=lang['code'])
+
+            obj.save()
+
+    def update_block_information(self, set_list):
+        for s in set_list:
+
+            set_data = s[1]
+            # Ignore sets that have no block
+            if 'block' not in set_data:
+                continue
+
+            obj = None
+            try:
+                obj = Block.objects.get(name=set_data['block'])
+
+            except Language.DoesNotExist:
+                obj = Language(name=set_data['block'], release_date=set_data['release_date'])
+
+            obj.save()
+
+    def update_set_information(self, set_list):
         pass
 
-    def update_block_information(self):
+    def update_card_information(self, set_list):
         pass
 
-    def update_set_information(self):
+    def update_ruling_table(self, set_list):
         pass
 
-    def update_card_information(self):
-        pass
-
-    def update_ruling_table(self):
-        pass
-
-    def update_physical_cards(self):
+    def update_physical_cards(self, set_list):
         pass
