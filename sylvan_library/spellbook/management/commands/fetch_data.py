f1fee481d4a04bdc4a8e1fe53b34fcf4c539bf96
marshl <marshl@live.com.au>
2016-08-20 12:36:04 +1000

    Trimmed trailing space from fetch_data.py

--- sylvan_library/spellbook/management/commands/fetch_data.py
+++ sylvan_library/spellbook/management/commands/fetch_data.py
@@ -14,29 +14,28 @@ def parse_json_data():
 
 class Command(BaseCommand):
     help = 'Downloads the MtG JSON data file'
-    
+
     def handle(self, *args, **options):
-        
+
         if path.isfile(_paths.json_data_path):
             overwrite = _query.query_yes_no(_paths.json_zip_path + ' already exists, overwrite?')
-            
+
             if not overwrite:
                 return
-            
+
         stream = requests.get(_paths.json_zip_download_url)
-        
+
         with open(_paths.json_zip_path, 'wb') as output:
             output.write(stream.content)
-        
+
         json_zip_file = zipfile.ZipFile(_paths.json_zip_path)
         json_zip_file.extractall(_paths.output_data_folder)
-        
+
         json_data = parse_json_data()
         pretty_file = open(_paths.pretty_json_path, 'w', encoding='utf8')
         pretty_file.write(json.dumps(json_data,
                            sort_keys=True,
                            indent=2,
                            separators=(',', ': ')))
-        
+
         pretty_file.close()
-        
