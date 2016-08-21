029db2bb20d03edeb38728c01ab8e098dfa810cd
marshl <marshl@live.com.au>
2016-08-21 11:38:50 +1000

    Added Set data updating

--- sylvan_library/spellbook/management/commands/update_database.py
+++ sylvan_library/spellbook/management/commands/update_database.py
@@ -62,9 +62,11 @@ class Command(BaseCommand):
             obj.save()
 
     def update_block_information(self, set_list):
+
         for s in set_list:
 
             set_data = s[1]
+
             # Ignore sets that have no block
             if 'block' not in set_data:
                 continue
@@ -79,7 +81,24 @@ class Command(BaseCommand):
             obj.save()
 
     def update_set_information(self, set_list):
-        pass
+
+        for s in set_list:
+
+            set_code = s[0]
+            set_data = s[1]
+            obj = None
+
+            try:
+                obj = Set.objects.get(code=set_code)
+
+            except Set.DoesNotExist:
+
+                b = Block.objects.filter(name=set_data.get('block')).first()
+
+                obj = Set(code=set_code, name=set_data['name'], release_date=set_data['releaseDate'], block=b, mci_code=set_data.get('magicCardsInfoCode'))
+
+                obj.save()
+
 
     def update_card_information(self, set_list):
         pass
