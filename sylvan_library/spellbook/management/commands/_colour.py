79e47d90193da129ae961baa96fa0e7c68b6e492
marshl <marshl@live.com.au>
2016-08-22 20:03:35 +1000

    Added a file for colour helper functions

---
+++ sylvan_library/spellbook/management/commands/_colour.py
@@ -0,0 +1,30 @@
+
+colour_name_to_flag = {
+    'white': 1,
+    'blue': 2,
+    'black': 4,
+    'red': 8,
+    'green': 16,
+}
+
+colour_code_to_flag = {
+    'w': 1,
+    'u': 2,
+    'b': 4,
+    'r': 8,
+    'g': 16,
+}
+
+def get_colour_flags_from_names(colour_names):
+    flags = 0
+    for colour in colour_names:
+        flags |= colour_name_to_flag[colour.lower()]
+
+    return flags
+
+def get_colour_flags_from_codes(colour_codes):
+    flags = 0
+    for colour in colour_codes:
+        flags |= colour_code_to_flag[colour.lower()]
+
+    return flags
