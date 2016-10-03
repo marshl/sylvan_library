ad664da8a07d9fb255df9e39f2c3f6d46ce053b0
marshl <marshl@live.com.au>
2016-10-03 12:29:29 +1100

    Added logging to the update_database command (and converted print statements to logging.info);

--- sylvan_library/spellbook/management/commands/update_database.py
+++ sylvan_library/spellbook/management/commands/update_database.py
@@ -1,7 +1,8 @@
-from django.core.management.base import BaseCommand
-
 import json
+import logging
 import re
+
+from django.core.management.base import BaseCommand
 
 from ...models import Card, CardPrinting, CardPrintingLanguage, PhysicalCard
 from ...models import PhysicalCardLink
@@ -20,15 +21,17 @@ class Command(BaseCommand):
            json_data.items(),
            key=lambda card_set: card_set[1]["releaseDate"])
 
-        self.update_rarity_table()
-        self.update_language_table()
-        self.update_block_table(json_data)
-        self.update_set_table(json_data)
-        self.update_card_table(json_data)
-        self.update_ruling_table(json_data)
-        self.update_physical_cards(json_data)
+        self.update_rarity_list()
+        self.update_language_list()
+        self.update_block_list(json_data)
+        self.update_set_list(json_data)
+        self.update_card_list(json_data)
+        self.update_ruling_list(json_data)
+        self.update_physical_card_list(json_data)
 
-    def update_rarity_table(self):
+    def update_rarity_list(self):
+
+        logging.info('Updating rarity list')
 
         f = open(_paths.rarity_json_path, 'r', encoding="utf8")
         rarities = json.load(f, encoding='UTF-8')
@@ -38,11 +41,16 @@ class Command(BaseCommand):
 
             obj = None
             try:
+
                 obj = Rarity.objects.get(symbol=r['symbol'])
+
+                logging.info('Updating rarity %s', obj.name)
                 obj.name = r['name']
                 obj.display_order = r['display_order']
 
             except Rarity.DoesNotExist:
+                logging.info('Creating new rarity %s', r['name'])
+
                 obj = Rarity(
                      symbol=r['symbol'],
                      name=r['name'],
@@ -50,7 +58,11 @@ class Command(BaseCommand):
 
             obj.save()
 
-    def update_language_table(self):
+        logging.info('Rarity update complete')
+
+    def update_language_list(self):
+
+        logging.info('Updating language list')
 
         f = open(_paths.language_json_path, 'r', encoding="utf8")
         languages = json.load(f, encoding='UTF-8')
@@ -61,14 +73,20 @@ class Command(BaseCommand):
             obj = None
             try:
                 obj = Language.objects.get(name=lang['name'])
+                logging.info('Updating language: %s', lang['name'])
                 obj.mci_code = lang['code']
 
             except Language.DoesNotExist:
+                logging.info('Creating new language: %s', lang['name'])
                 obj = Language(name=lang['name'], mci_code=lang['code'])
 
             obj.save()
 
-    def update_block_table(self, set_list):
+        logging.info('Language update complete')
+
+    def update_block_list(self, set_list):
+
+        logging.info('Updating block list')
 
         for s in set_list:
 
@@ -76,53 +94,74 @@ class Command(BaseCommand):
 
             # Ignore sets that have no block
             if 'block' not in set_data:
+                logging.info('Ignoring %s', set_data['name'])
                 continue
 
             obj = None
             try:
                 obj = Block.objects.get(name=set_data['block'])
+                logging.info('Block %s already exists', obj.name)
 
             except Block.DoesNotExist:
                 obj = Block(
                     name=set_data['block'],
                     release_date=set_data['releaseDate'])
+
+                logging.info('Created block %s', obj.name)
                 obj.save()
 
-    def update_set_table(self, set_list):
+        logging.info('BLock list updated')
+
+    def update_set_list(self, set_list):
+
+        logging.info('Updating set list')
 
         for s in set_list:
 
             set_code = s[0]
+            set_data = s[1]
 
             # Skip sets that start with 'p' (e.g. pPRE Prerelease Events)
             if set_code[0] == 'p':
+                logging.info('Ignoring set %s', set_data['name'])
                 continue
 
-            set_data = s[1]
             set_obj = None
 
             if not Set.objects.filter(code=set_code).exists():
 
+                logging.info('Creating set %s', set_data['name'])
                 b = Block.objects.filter(name=set_data.get('block')).first()
+
                 set_obj = Set(
                   code=set_code,
                   name=set_data['name'],
                   release_date=set_data['releaseDate'],
                   block=b,
                   mci_code=set_data.get('magicCardsInfoCode'))
-                set_obj.save()
 
-    def update_card_table(self, set_list):
+                set_obj.save()
+            else:
+                logging.info('Set %s already exists, no changes made',
+                             set_data['name'])
+
+        logging.info('Set list updated')
+
+    def update_card_list(self, set_list):
+
+        logging.info('Updating card list')
 
         for s in set_list:
 
             set_code = s[0]
             set_data = s[1]
-
             default_cnum = 0
 
             if not Set.objects.filter(code=set_code).exists():
+                logging.info('Ignoring set "%s"', set_data['name'])
                 continue
+
+            logging.info('Updating cards in set "%s"', set_data['name'])
 
             set_obj = Set.objects.get(code=set_code)
 
@@ -149,47 +188,58 @@ class Command(BaseCommand):
 
                         self.update_card_printing_language(printing_obj, lang)
 
+        logging.info('Card list updated')
+
     def update_card(self, card_data):
 
         card = None
-
         card_name = self.get_card_name(card_data)
 
         try:
             card = Card.objects.get(name=card_name)
+            logging.info('Updating existing card "%s"', card)
 
         except Card.DoesNotExist:
-
             card = Card(name=card_name)
+            logging.info('Creating new card "%s"', card)
 
         card.cost = card_data.get('manaCost')
         card.cmc = card_data.get('cmc') or 0
-        card.colour = 0
+
         if 'colors' in card_data:
             card.card_colour = _colour.get_colour_flags_from_names(
                                    card_data['colors'])
+        else:
+            card.colour = 0
 
         card.colour_identity = 0
         if 'colourIdentity' in card_data:
             card.colour_identity = _colour.get_colour_flags_from_codes(
                                             card_data['colorIdentity'])
+        else:
+            card.colour_identity = 0
 
         card.colour_count = bin(card.colour).count('1')
 
         card.power = card_data.get('power')
         card.toughness = card_data.get('toughness')
-        card.num_power = 0
+
         if 'power' in card_data:
             card.num_power = self.convert_to_number(card_data['power'])
+        else:
+            card.num_power = 0
 
-        card.num_toughness = 0
         if 'toughness' in card_data:
             card.num_toughness = self.convert_to_number(card_data['toughness'])
+        else:
+            card.num_toughness = 0
 
         card.loyalty = card_data.get('loyalty')
-        card.num_loyalty = 0
+
         if 'loyalty' in card_data:
             card.num_loyalty = self.convert_to_number(card_data['loyalty'])
+        else:
+            card.num_loyalty = 0
 
         if 'types' in card_data:
 
@@ -222,18 +272,21 @@ class Command(BaseCommand):
                         set=set_obj,
                         collector_number=cnum,
                         collector_letter=cnum_letter)
+            logging.info('Updating card printing "%s"', printing)
 
         except CardPrinting.DoesNotExist:
+
             printing = CardPrinting(
                             card=card_obj,
                             set=set_obj,
                             collector_number=cnum,
                             collector_letter=cnum_letter)
+            logging.info('Created new card priting "%s"', printing)
 
         printing.artist = card_data['artist']
 
         rarity_name = card_data['rarity']
-        if 'timeshifted' in card_data:
+        if 'timeshifted' in card_data and card_data['timeshifted']:
             rarity_name = 'Timeshifted'
 
         printing.rarity = Rarity.objects.get(name=rarity_name)
@@ -263,10 +316,14 @@ class Command(BaseCommand):
                         card_printing=printing_obj,
                         language=lang_obj)
 
+            logging.info('Updating printing language %s', cardlang)
+
         except CardPrintingLanguage.DoesNotExist:
             cardlang = CardPrintingLanguage(
                         card_printing=printing_obj,
                         language=lang_obj)
+
+            logging.info('Creating new printing language "%s"', cardlang)
 
         cardlang.multiverse_id = lang.get('multiverseid')
         cardlang.card_name = lang['name']
@@ -282,17 +339,20 @@ class Command(BaseCommand):
 
         return 0
 
-    def update_ruling_table(self, set_list):
+    def update_ruling_list(self, set_list):
 
+        logging.info('Updating card rulings')
         CardRuling.objects.all().delete()
 
         for s in set_list:
 
             set_code = s[0]
+            set_data = s[1]
             if not Set.objects.filter(code=set_code).exists():
+                logging.info('Ignoring set "%s"', set_data['name'])
                 continue
 
-            set_data = s[1]
+            logging.info('Updating ruligns in "%s"', set_data['name'])
 
             for card_data in set_data['cards']:
 
@@ -300,8 +360,9 @@ class Command(BaseCommand):
                     continue
 
                 card_name = self.get_card_name(card_data)
-
                 card_obj = Card.objects.get(name=card_name)
+
+                logging.info('Updating rulings for "%s"', card_name)
 
                 for ruling in card_data['rulings']:
 
@@ -318,7 +379,11 @@ class Command(BaseCommand):
                                         date=ruling['date'])
                         ruling_obj.save()
 
-    def update_physical_cards(self, set_list):
+        logging.info('Card rulings updated')
+
+    def update_physical_card_list(self, set_list):
+
+        logging.info('Updating physical card list')
 
         for s in set_list:
 
@@ -326,6 +391,7 @@ class Command(BaseCommand):
             set_data = s[1]
 
             if not Set.objects.filter(code=set_code).exists():
+                logging.info('Skipping set "%s"', set_data['name'])
                 continue
 
             set_obj = Set.objects.get(code=set_code)
@@ -333,15 +399,17 @@ class Command(BaseCommand):
             default_cnum = 0
 
             for card_data in set_data['cards']:
+
+                card_name = self.get_card_name(card_data)
+
+                logging.info('Updating physical cards for %s', card_name)
+                card_obj = Card.objects.get(name=card_name)
+
                 default_cnum += 1
 
                 (cnum, cnum_letter) = self.get_card_cnum(
                                          card_data,
                                          default_cnum)
-
-                card_name = self.get_card_name(card_data)
-
-                card_obj = Card.objects.get(name=card_name)
 
                 printing_obj = CardPrinting.objects.get(
                                     card=card_obj,
@@ -356,7 +424,7 @@ class Command(BaseCommand):
 
                 self.update_physical_card(printlang_obj, card_data)
 
-                if 'foreignNames' in card_data and False:
+                if 'foreignNames' in card_data:
 
                     for card_language in card_data['foreignNames']:
 
@@ -371,20 +439,21 @@ class Command(BaseCommand):
 
     def update_physical_card(self, printlang_obj, card_data):
 
+        logging.info('Updating physical cards for "%s"', printlang_obj)
+
         if (card_data['layout'] == 'meld' and
                 len(card_data['names']) == 3 and
                 printlang_obj.card_printing.collector_letter == 'b'):
 
-            print(('Will not create card link for {0}')
-                  .format(printlang_obj.card_name)
-                  .encode('utf-8'))
+            logging.info('Will not create card link for meld card "%s',
+                         printlang_obj)
 
             return
 
         if (PhysicalCardLink.objects.filter(printing_language=printlang_obj)
-                .exists()):
-            print('Physical link already exists for {0}'
-                  .format(printlang_obj.card_name))
+           .exists()):
+            logging.info('Physical link already exists for "%s"',
+                         printlang_obj)
 
             return
 
@@ -406,8 +475,8 @@ class Command(BaseCommand):
                         printlang_obj.card_printing.collector_letter != 'b' and
                         link_print.collector_letter != 'b'):
 
-                    print('Will not link {0} to {1} as they separate cards'
-                          .format(card_data['name'], link_card.name))
+                    logging.info('Won''t link %s to %s as they separate cards',
+                                 card_data['name'], link_card.name)
 
                     continue
 
