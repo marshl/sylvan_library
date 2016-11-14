64ec918aea91143f83eafe7f11a351d56c72c74f
marshl <marshl@live.com.au>
2016-11-14 19:23:15 +1100

    Tidied up views.py

--- sylvan_library/spellbook/views.py
+++ sylvan_library/spellbook/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Card, Set
+from spellbook.models import Card, Set
 from django.shortcuts import get_object_or_404
 
-# Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+
 def card_detail(request, card_id):
-    #card = Card.objects.get(id=card_id)
     card = get_object_or_404(Card, pk=card_id)
     context = {'card': card}
     return render(request, 'spellbook/card_detail.html', context)
-    # return HttpResponse("You're looking for card {0}".format(Card.objects.get(id=card_id)))
 
 
 def set_detail(request, set_code):
-    
+
     set_obj = get_object_or_404(Set, code=set_code)
-    context = { 'set': set_obj }
+    context = {'set': set_obj}
     return render(request, 'spellbook/set.html', context)
