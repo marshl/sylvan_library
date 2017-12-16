f827311806b10d21a53d8b1aca91054116416b35
marshl <marshl@live.com.au>
2017-12-16 22:09:02 +1100

    Added a test for StagedCard type collation

--- sylvan_library/data_import/tests.py
+++ sylvan_library/data_import/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase
+from data_import.staging import *
+
+
+class StagedCardTestCase(TestCase):
+    def test_get_type(self):
+        staged_card = StagedCard({'types': ['Legendary', 'Creature']})
+        self.assertEqual(staged_card.get_types(), 'Legendary Creature')
