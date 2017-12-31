8cf9a3f3d290e5db5c489570db09c351a127adb4
marshl <marshl@live.com.au>
2017-12-31 22:55:01 +1100

    Added better local logging

--- sylvan_library/data_import/_paths.py
+++ sylvan_library/data_import/_paths.py
@@ -1,6 +1,4 @@
 from os import path
-from datetime import datetime
-import logging
 
 json_zip_download_url = "http://mtgjson.com/json/AllSets-x.json.zip"
 data_folder = path.abspath('data_import/data')
@@ -10,11 +8,3 @@ pretty_json_path = path.join(data_folder, 'AllSets-x-pretty.json')
 
 language_json_path = path.join(data_folder, 'languages.json')
 rarity_json_path = path.join(data_folder, 'rarities.json')
-
-log_file_path = path.abspath('data_import' +
-                             datetime.now().strftime('%Y_%m_%d') +
-                             '.log')
-
-logging.basicConfig(filename=log_file_path,
-                    level=logging.DEBUG,
-                    format='%(levelname)s %(asctime)s %(message)s')
--- sylvan_library/sylvan_library/settings/local.py
+++ sylvan_library/sylvan_library/settings/local.py
@@ -1,14 +1,15 @@
 # Settings file used for local development.
 from __future__ import absolute_import
+from datetime import datetime
 from .base import *
 
 DEBUG = True
-#JIRA_ISSUE_COLLECTOR = False
+# JIRA_ISSUE_COLLECTOR = False
 ENABLE_VIRUS_SCAN = False
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = ['*', ]
 
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_REPLY_TO_DOMAIN = 'localhost'
 EMAIL_FROM_ADDRESS = 'recruitment@localhost'
@@ -19,7 +20,7 @@ SERVER_URL = 'http://localhost:8000'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-#MEDIA_ROOT = BASE_DIR.child('media')
+# MEDIA_ROOT = BASE_DIR.child('media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -41,25 +42,41 @@ STATIC_URL = '/static/'
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
+
+log_file = path.abspath(__name__ + datetime.now().strftime('%Y_%m_%d') + '.log')
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '%(message)s',
+        },
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(message)s',
+        },
+    },
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
         }
     },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': log_file,
+            'formatter': 'verbose',
+        },
     },
     'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
+        'django': {
+            'handlers': ['console', 'file'],
             'propagate': True,
         },
     }
@@ -70,17 +87,20 @@ LOGGING = {
 # The following function adds wildcard support, as taken from:
 # http://dancarroll.org/blog/2011/01/debugging-django-dev-server/
 from fnmatch import fnmatch
+
+
 class glob_list(list):
     def __contains__(self, key):
         for elt in self:
             if fnmatch(key, elt): return True
         return False
 
+
 # Required for django-debug-toolbar. When connecting to the recruitment system
 # using the following IPs the debug-toolbar will be displayed.
 INTERNAL_IPS = glob_list(['127.0.0.1', '10.0.*.*'])
 
 INSTALLED_APPS += (
-   # 'debug_toolbar',
-    #'template_timings_panel',
+    # 'debug_toolbar',
+    # 'template_timings_panel',
 )
