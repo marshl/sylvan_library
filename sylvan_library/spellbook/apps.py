5d54de5a90abed9ea22cf6acc2ee29a51781f332
marshl <marshl@live.com.au>
2018-02-20 23:18:11 +1100

    Removed the defunct spellbook project (was split into site/cards/data_import)

--- sylvan_library/spellbook/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpellbookConfig(AppConfig):
-    name = 'spellbook'
--- sylvan_library/spellbook/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from spellbook import views
-
-app_name = 'spellbook'
-urlpatterns = [
-   url(r'^$', views.index, name='index'),
-]
