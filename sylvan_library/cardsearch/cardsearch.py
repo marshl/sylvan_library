e4216503b2364651271c21c0d262a48740c96e61
marshl <marshl@live.com.au>
2018-03-20 19:56:14 +1100

    Added a search parameter for card mana costs

--- sylvan_library/cardsearch/cardsearch.py
+++ sylvan_library/cardsearch/cardsearch.py
@@ -9,7 +9,7 @@ from cards.models import *
 comparisons = {'<': '__lt', '<=': '__lte', '>': '__gt', '>=': '__gte', '=': ''}
 
 
-class SearchParameterNode:
+class CardSearchParam:
     def __init__(self):
         self.child_parameters = list()
         self.boolean_flag = True
@@ -18,12 +18,12 @@ class SearchParameterNode:
         raise NotImplementedError('Please implement this method')
 
 
-class BranchParameterNode(SearchParameterNode):
+class BranchParameterNode(CardSearchParam):
     def __init__(self):
         super().__init__()
         self.child_parameters = list()
 
-    def add_parameter(self, param: SearchParameterNode):
+    def add_parameter(self, param: CardSearchParam):
         self.child_parameters.append(param)
         return param
 
@@ -58,7 +58,7 @@ class OrParameterNode(BranchParameterNode):
         return result
 
 
-class CardNameSearchParam(SearchParameterNode):
+class CardNameSearchParam(CardSearchParam):
     def __init__(self, card_name):
         super().__init__()
         self.card_name = card_name
@@ -70,7 +70,7 @@ class CardNameSearchParam(SearchParameterNode):
             return Card.objects.exclude(name__icontains=self.card_name)
 
 
-class CardRulesSearchParam(SearchParameterNode):
+class CardRulesSearchParam(CardSearchParam):
     def __init__(self, card_rules):
         super().__init__()
         self.card_rules = card_rules
@@ -82,7 +82,7 @@ class CardRulesSearchParam(SearchParameterNode):
             return Card.objects.exclude(rules_text__icontains=self.card_rules)
 
 
-class CardTypeParam(SearchParameterNode):
+class CardTypeParam(CardSearchParam):
     def __init__(self, card_type):
         super().__init__()
         self.card_type = card_type
@@ -94,7 +94,7 @@ class CardTypeParam(SearchParameterNode):
             return Card.objects.exclude(type__icontains=self.card_type)
 
 
-class CardSubtypeParam(SearchParameterNode):
+class CardSubtypeParam(CardSearchParam):
     def __init__(self, card_subtype):
         super().__init__()
         self.card_subtype = card_subtype
@@ -106,7 +106,7 @@ class CardSubtypeParam(SearchParameterNode):
             return Card.objects.exclude(subtype__icontains=self.card_subtype)
 
 
-class CardColourParam(SearchParameterNode):
+class CardColourParam(CardSearchParam):
     def __init__(self, card_colour: Bit):
         super().__init__()
         self.card_colour = card_colour
@@ -118,7 +118,7 @@ class CardColourParam(SearchParameterNode):
             return Card.objects.exclude(colour_flags=self.card_colour)
 
 
-class CardColourIdentityParam(SearchParameterNode):
+class CardColourIdentityParam(CardSearchParam):
     def __init__(self, colour_identity: Bit):
         super().__init__()
         self.colour_identity = colour_identity
@@ -130,7 +130,7 @@ class CardColourIdentityParam(SearchParameterNode):
             return Card.objects.exclude(colour_identity_flags=self.colour_identity)
 
 
-class CardMulticolouredOnlyParam(SearchParameterNode):
+class CardMulticolouredOnlyParam(CardSearchParam):
     def __init__(self):
         super().__init__()
 
@@ -141,7 +141,7 @@ class CardMulticolouredOnlyParam(SearchParameterNode):
             return Card.objects.filter(colour_count__lte=1)
 
 
-class CardSetParam(SearchParameterNode):
+class CardSetParam(CardSearchParam):
     def __init__(self, set_obj: Set):
         super().__init__()
         self.set_obj = set_obj
@@ -153,7 +153,7 @@ class CardSetParam(SearchParameterNode):
             return Card.objects.exclude(printings__set=self.set_obj)
 
 
-class CardBlockParam(SearchParameterNode):
+class CardBlockParam(CardSearchParam):
     def __init__(self, block_obj: Block):
         super().__init__()
         self.block_obj = block_obj
@@ -165,7 +165,7 @@ class CardBlockParam(SearchParameterNode):
             return Card.objects.exclude(printings__set__block=self.block_obj)
 
 
-class CardOwnerParam(SearchParameterNode):
+class CardOwnerParam(CardSearchParam):
     def __init__(self, user: User):
         super().__init__()
         self.user = user
@@ -177,7 +177,23 @@ class CardOwnerParam(SearchParameterNode):
             return Card.objects.exclude(printings__printed_languages__physical_cards__ownerships__owner=self.user)
 
 
-class CardNumericalParam(SearchParameterNode):
+class CardManaCostParam(CardSearchParam):
+    def __init__(self, cost: str, exact_match: bool):
+        super().__init__()
+        self.cost = cost
+        self.exact_match = exact_match
+
+    def get_result(self):
+
+        q = Q(cost=self.cost) if self.exact_match else Q(cost__icontains=self.cost)
+
+        if self.boolean_flag:
+            return Card.objects.filter(q)
+        else:
+            return Card.objects.exclude(q)
+
+
+class CardNumericalParam(CardSearchParam):
 
     def __init__(self, number: int, operator: str):
         super().__init__()
