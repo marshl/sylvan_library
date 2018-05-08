d36d53dac3dcf02bada3d7ef037706af1941d530
marshl <marshl@live.com.au>
2018-05-08 22:19:49 +1000

    Removed a debug .bat file

--- sylvan_library/import_from_delverdb.bat
+++
@@ -1,5 +0,0 @@
-php C:\wamp\www\delverdb\dump.php cardchanges > cardchanges.txt
-php C:\wamp\www\delverdb\dump.php usercards > usercards.txt
-
-python manage.py import_usercards Liam usercards.txt
-python manage.py import_usercardchanges Liam cardchanges.txt
