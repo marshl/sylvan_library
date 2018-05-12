5d5fcae93ed1263a6088918fa09af7156f999c4d
marshl <marshl@live.com.au>
2018-05-13 00:10:28 +1000

    Removed the unused colour widget

--- sylvan_library/website/forms.py
+++ sylvan_library/website/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from cardsearch import parameters
-from website.templates.widgets.colourwidget import ColourWidget, ColourField
 
 
 class SearchForm(forms.Form):
@@ -8,10 +7,6 @@ class SearchForm(forms.Form):
     rules_text = forms.CharField(required=False)
     cmc = forms.IntegerField(required=False)
     cmc_operator = forms.ChoiceField(parameters.NUMERICAL_OPERATOR_CHOICES, required=False)
-    # colours = ColourWidget()
-    # colours = forms.MultipleChoiceField(widget=ColourWidget, required=False)
-    #colours = ColourField()
-    # colours = forms.CheckboxSelectMultiple(choices=(('WHITE','W'), ('BLUE', 'U')))
 
     colour_white = forms.BooleanField(required=False)
     colour_blue = forms.BooleanField(required=False)
