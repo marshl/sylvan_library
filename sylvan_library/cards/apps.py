6204c1b955a52d236b3fc80d83e60d0d9b0f6198
marshl <marshl@live.com.au>
2019-01-19 21:12:22 +1100

    Linted cards/apps.py

--- sylvan_library/cards/apps.py
+++ sylvan_library/cards/apps.py
@@ -1,5 +1,11 @@
+"""
+Configuration for the cards app
+"""
 from django.apps import AppConfig
 
 
 class CardsConfig(AppConfig):
+    """
+    Configuration for the cards app
+    """
     name = 'cards'
