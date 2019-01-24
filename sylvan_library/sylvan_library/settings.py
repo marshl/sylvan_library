fe859494b7daa93b29fd8c90352ea8590d37139d
marshl <marshl@live.com.au>
2019-01-24 20:44:18 +1100

    Added missing middleware

--- sylvan_library/sylvan_library/settings.py
+++ sylvan_library/sylvan_library/settings.py
@@ -42,6 +42,14 @@ INSTALLED_APPS = [
     'widget_tweaks',
 ]
 
+MIDDLEWARE = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
