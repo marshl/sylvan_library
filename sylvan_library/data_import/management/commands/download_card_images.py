2910ef4c6ed3b397a9826ca330002d343450d468
marshl <marshl@live.com.au>
2019-02-11 21:49:43 +1100

    Changed image downloading to use threading again

--- sylvan_library/data_import/management/commands/download_card_images.py
+++ sylvan_library/data_import/management/commands/download_card_images.py
@@ -4,6 +4,8 @@ Module for the download_card_images command
 import os
 import logging
 import random
+import queue
+import threading
 import time
 import requests
 
@@ -48,14 +50,19 @@ class Command(BaseCommand):
                 'object does not exist. Please run `update_database` first')
             return
 
+        image_download_queue = queue.Queue()
+
+        for i in range(1, self.download_thread_count):
+            thread = ImageDownloadThread(image_download_queue)
+            thread.setDaemon(True)
+            thread.start()
+
         for printing in CardPrinting.objects.all():
             if not printing.scryfall_id:
                 continue
 
-            image_uri = None
-
+            base_image_uri = None
             logger.info(f'Downloading images for {printing}')
-
             for printed_language in printing.printed_languages.all():
                 if printed_language.language.code is None:
                     continue
@@ -70,31 +77,54 @@ class Command(BaseCommand):
                     logger.info(f'\tSkipping {printed_language}')
                     card_image = CardImage(printed_language=printed_language, downloaded=True)
                     card_image.save()
+
                     continue
 
-                if image_uri is None:
+                if base_image_uri is None:
                     url = 'https://api.scryfall.com/cards/' + printing.scryfall_id
                     resp = requests.get(url=url)
                     data = resp.json()
-                    image_uri = data['image_uris']['normal']
-
-                localised_image_uri = image_uri.replace('/en/',
-                                                        '/' + printed_language.language.code + '/')
-                stream = requests.get(localised_image_uri)
-                try:
-                    stream.raise_for_status()
-                except requests.exceptions.HTTPError:
-                    logger.warning(
-                        f'\tCould not download {printed_language} ({localised_image_uri})')
-                    card_image = CardImage(printed_language=printed_language, downloaded=False)
-                    card_image.save()
-                else:
-                    os.makedirs(os.path.dirname(image_path), exist_ok=True)
-                    with open(image_path, 'wb') as output:
-                        output.write(stream.content)
-
-                    card_image = CardImage(printed_language=printed_language, downloaded=True)
-                    card_image.save()
-                    logger.info(f'\tDownloaded {printed_language}')
-                finally:
+                    base_image_uri = data['image_uris']['normal']
+                    # Sleep after every request made to reduce server load
                     time.sleep(random.random() * 0.25 + 0.1)
+
+                localised_image_uri = base_image_uri.replace(
+                    '/en/',
+                    f'/{printed_language.language.code}/')
+                image_download_queue.put((printed_language.id, localised_image_uri, image_path))
+
+        image_download_queue.join()
+
+
+class ImageDownloadThread(threading.Thread):
+    """
+    The thread object for downloading a card image
+    """
+
+    def __init__(self, download_queue):
+        threading.Thread.__init__(self)
+        self.download_queue = download_queue
+
+    def run(self):
+        while True:
+            (printed_language_id, download_url, image_path) = self.download_queue.get()
+
+            printed_language = CardPrintingLanguage.objects.get(id=printed_language_id)
+            stream = requests.get(download_url)
+            try:
+                stream.raise_for_status()
+            except requests.exceptions.HTTPError:
+                logger.warning(
+                    f'\tCould not download {printed_language} ({download_url})')
+                card_image = CardImage(printed_language=printed_language, downloaded=False)
+                card_image.save()
+            else:
+                os.makedirs(os.path.dirname(image_path), exist_ok=True)
+                with open(image_path, 'wb') as output:
+                    output.write(stream.content)
+
+                card_image = CardImage(printed_language=printed_language, downloaded=True)
+                card_image.save()
+                logger.info(f'\tDownloaded {printed_language}')
+
+            self.download_queue.task_done()
