7368650a6181e8026113425e99f74b2b581db162
marshl <marshl@live.com.au>
2019-02-17 17:00:55 +1100

    Split basic land into its own rarity in the rarity report and fixed some issue with counting for erroneous decks

--- sylvan_library/reports/management/commands/generate_deck_rarity_report.py
+++ sylvan_library/reports/management/commands/generate_deck_rarity_report.py
@@ -19,10 +19,13 @@ class Command(BaseCommand):
     help = 'Verifies that database update was successful'
 
     def __init__(self):
-        self.rarities = ['C', 'U', 'R', 'M']
+        self.rarities = ['L', 'C', 'U', 'R', 'M']
         super().__init__()
 
     def handle(self, *args, **options):
+        output_path = os.path.join('reports', 'output', 'deck_rarity_progression.png')
+        os.remove(output_path)
+
         owner = User.objects.get(username='Test')
         decks = Deck.objects.filter(owner=owner).prefetch_related('cards__card__printings__set')
         dates = [d['date_created'] for d in decks.values('date_created').distinct()]
@@ -30,42 +33,49 @@ class Command(BaseCommand):
         for dt in dates:
             date_decks = decks.filter(date_created=dt)
             row = [0] * len(self.rarities)
+            deck_count = 0
             for deck in date_decks:
+                if sum(x.count for x in deck.cards.all()) < 60:
+                    continue
+
                 r = self.get_deck_rarity_ratios(deck)
                 for idx, rarity in enumerate(self.rarities):
                     row[idx] += r[rarity]
 
-            row = [x / len(date_decks) for x in row]
-            rows.append(row)
+                deck_count += 1
+
+            if deck_count > 0:
+                row = [x / deck_count for x in row]
+                rows.append(row)
 
         sns.set(style="whitegrid")
-
-        sns.set(rc={'figure.figsize': (12, 8)})
+        sns.set(rc={'figure.figsize': (10, 6)})
         sns.set(color_codes=True)
 
         date_index = pd.DatetimeIndex(dates)
         data = pd.DataFrame(rows, index=date_index, columns=self.rarities)
-        data = data.resample('90D').mean()
+        data = data.resample('180D').mean()
         data = data.interpolate(method='cubic')
-
-        plt = sns.lineplot(data=data, palette={'C': '#0E0C0C', 'U': '#8A8D91','R': '#C1A15B','M': '#EC7802',}, linewidth=1.5, hue='A')
+        palette = {'L': '#875438', 'C': '#0E0C0C', 'U': '#8A8D91', 'R': '#C1A15B', 'M': '#EC7802'}
+        plt = sns.lineplot(data=data, palette=palette, linewidth=1.5, hue='A')
         fig = plt.figure
 
-        fig.savefig(os.path.join('reports', 'output', 'test.png'))
+        fig.savefig(output_path)
 
     def get_deck_rarity_ratios(self, deck: Deck) -> dict:
         counts = {r: 0 for r in self.rarities}
         total_count = 0
         for deck_card in deck.cards.all():
-            #if 'Land' in deck_card.card.type:
-            #    continue
-            closest_printing = deck_card.card.printings.first()
-            for printing in deck_card.card.printings.all():
-                if abs(deck.date_created - printing.set.release_date) \
-                        < abs(deck.date_created - closest_printing.set.release_date):
-                    closest_printing = printing
+            if 'Basic' in deck_card.card.type:
+                counts['L'] += deck_card.count
+            else:
+                closest_printing = deck_card.card.printings.first()
+                for printing in deck_card.card.printings.all():
+                    if abs(deck.date_created - printing.set.release_date) \
+                            < abs(deck.date_created - closest_printing.set.release_date):
+                        closest_printing = printing
 
-            counts[closest_printing.rarity.symbol] += deck_card.count
+                counts[closest_printing.rarity.symbol] += deck_card.count
             total_count += deck_card.count
 
         ratios = {key: value / total_count for key, value in counts.items()}
