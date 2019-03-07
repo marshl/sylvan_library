ff1126b923e1470f1a30dabbe0620e836508c20d
marshl <marshl@live.com.au>
2019-03-07 11:35:10 +1100

    Fixed the unit tests that were broken by the change in how the search parameters work

--- sylvan_library/cardsearch/tests.py
+++ sylvan_library/cardsearch/tests.py
@@ -4,7 +4,7 @@ The module for searching tests
 
 from django.test import TestCase
 
-from cards.models import Set
+from cards.models import Card, Set
 from cards.tests import (
     create_test_card,
     create_test_card_printing,
@@ -13,8 +13,8 @@ from cards.tests import (
 
 
 from cardsearch.parameters import (
+    AndParam,
     CardNameParam,
-    NotParam,
     CardRulesTextParam,
     CardSetParam,
 )
@@ -27,24 +27,24 @@ class CardNameParamTestCase(TestCase):
     def test_name_match(self):
         card = create_test_card({'name': 'foo'})
         param = CardNameParam('foo')
-        self.assertIn(card, param.query())
+        self.assertIn(card, Card.objects.filter(param.query()))
 
     def test_name_contains(self):
         card = create_test_card({'name': 'foobar'})
         param = CardNameParam('foo')
-        self.assertIn(card, param.query())
+        self.assertIn(card, Card.objects.filter(param.query()))
 
     def test_name_not_contains(self):
         card = create_test_card({'name': 'foo'})
         param = CardNameParam('bar')
-        self.assertNotIn(card, param.query())
+        self.assertNotIn(card, Card.objects.filter(param.query()))
 
     def test_name_match_invert(self):
-        root_param = NotParam()
+        root_param = AndParam(True)
         card = create_test_card({'name': 'foo'})
         param = CardNameParam('bar')
         root_param.add_parameter(param)
-        self.assertIn(card, root_param.query())
+        self.assertIn(card, Card.objects.filter(root_param.query()))
 
 
 class CardRulesParamTestCase(TestCase):
@@ -55,17 +55,17 @@ class CardRulesParamTestCase(TestCase):
     def test_rules_match(self):
         card = create_test_card({'rules_text': 'Flying'})
         param = CardRulesTextParam('Flying')
-        self.assertIn(card, param.query())
+        self.assertIn(card, Card.objects.filter(param.query()))
 
     def test_rules_contains(self):
         card = create_test_card({'rules_text': 'Double Strike'})
         param = CardRulesTextParam('strike')
-        self.assertIn(card, param.query())
+        self.assertIn(card, Card.objects.filter(param.query()))
 
     def test_rules_blank(self):
         card = create_test_card({})
         param = CardRulesTextParam('Vigilance')
-        self.assertNotIn(card, param.query())
+        self.assertNotIn(card, Card.objects.filter(param.query()))
 
 
 class CardSetParamTestCase(TestCase):
@@ -78,4 +78,4 @@ class CardSetParamTestCase(TestCase):
         set_obj = create_test_set('Foobar', 'FOO', {})
         create_test_card_printing(card, set_obj, {})
         param = CardSetParam(Set.objects.get(code='FOO'))
-        self.assertIn(card, param.query())
+        self.assertIn(card, Card.objects.filter(param.query()))
