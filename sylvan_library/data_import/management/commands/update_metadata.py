63db5c77a69c1c2a584dd4a8c4f85500aa5e9f66
marshl <marshl@live.com.au>
2019-07-23 14:14:17 +1000

    Combined the old "DataImportCommand" into the update_metadata command (which was the only command that used it) and removed the defunct JsonImporters class

--- sylvan_library/data_import/management/commands/update_metadata.py
+++ sylvan_library/data_import/management/commands/update_metadata.py
@@ -3,8 +3,10 @@ Module for the update_metadata command
 """
 import json
 import logging
+import time
 
 from django.db import transaction
+from django.core.management.base import BaseCommand
 
 from cards.models import Colour, Format, Language, Rarity
 from data_import._paths import (
@@ -14,7 +16,6 @@ from data_import._paths import (
     FORMAT_JSON_PATH,
 )
 
-from data_import.management.data_import_command import DataImportCommand
 
 logger = logging.getLogger("django")
 
@@ -29,7 +30,7 @@ def import_json(file_path: str) -> dict:
         return json.load(json_file, encoding="UTF-8")
 
 
-class Command(DataImportCommand):
+class Command(BaseCommand):
     """
     The command for updating all metadata in the database
     (data that is stored in external files, and not in mtgjson)
@@ -39,6 +40,14 @@ class Command(DataImportCommand):
         "The command for updating all metadata in the database "
         "(data that is stored in external files, and not in mtgjson)"
     )
+
+    def __init__(self, stdout=None, stderr=None, no_color=False):
+        self.update_counts = dict()
+        self.created_counts = dict()
+        self.ignored_counts = dict()
+        self.start_time = time.time()
+
+        super().__init__(stdout=stdout, stderr=stderr, no_color=no_color)
 
     def handle(self, *args, **options):
         with transaction.atomic():
@@ -139,3 +148,52 @@ class Command(DataImportCommand):
             format_obj.name = fmt["name"]
             format_obj.full_clean()
             format_obj.save()
+
+    def increment_updated(self, object_type: str):
+        """
+        Increments the number of objects that were updated
+        :param object_type: The type of object that was updated
+        :param object_type:
+        """
+        if object_type not in self.update_counts:
+            self.update_counts[object_type] = 0
+
+        self.update_counts[object_type] += 1
+
+    def increment_created(self, object_type: str):
+        """
+        Increments the number of objects that were created
+        :param object_type: The type of object that was created
+        """
+        if object_type not in self.created_counts:
+            self.created_counts[object_type] = 0
+
+        self.created_counts[object_type] += 1
+
+    def increment_ignores(self, object_type: str):
+        """
+        Increments the number of objects that were ignored
+        :param object_type: The type of object that was ignored
+        """
+        if object_type not in self.ignored_counts:
+            self.ignored_counts[object_type] = 0
+
+        self.ignored_counts[object_type] += 1
+
+    def log_stats(self):
+        """
+        Logs all updated/created/ignored objects
+        """
+        logger.info("%s", "\n" + ("=" * 80) + "\n\nUpdate complete:\n")
+        elapsed_time = time.time() - self.start_time
+        logger.info(
+            "Time elapsed: %s", time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
+        )
+        for key, value in self.created_counts.items():
+            logger.info("Created %s %s", key, value)
+
+        for key, value in self.update_counts.items():
+            logger.info("Updated %s %s", key, value)
+
+        for key, value in self.ignored_counts.items():
+            logger.info("Ignored %s %s", key, value)
