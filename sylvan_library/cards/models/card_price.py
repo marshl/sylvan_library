9367e6886fa42d3f512b6ac344e7157ba3cdfca8
marshl <marshl@live.com.au>
2019-11-09 12:45:27 +1100

    Added new price types and frame effects

--- sylvan_library/cards/models/card_price.py
+++ sylvan_library/cards/models/card_price.py
@@ -5,7 +5,12 @@ from cards.models import CardPrinting
 
 class CardPrice(models.Model):
 
-    PRICE_TYPE_CHOICES = (("paper", "Paper"), ("paper_foil", "Paper Foil"))
+    PRICE_TYPE_CHOICES = (
+        ("paper", "Paper"),
+        ("paper_foil", "Paper Foil"),
+        ("mtgo", "MTGO"),
+        ("mtgo_foil", "MTGO Foil"),
+    )
 
     printing = models.ForeignKey(
         CardPrinting, on_delete=models.CASCADE, related_name="prices"
