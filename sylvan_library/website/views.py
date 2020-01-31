8d80015f54cab763bc2ecd2859ce4c8e8d041236
marshl <marshl@live.com.au>
2020-01-31 22:41:22 +1100

    Disabled deck validation for prototype decks

--- sylvan_library/website/views.py
+++ sylvan_library/website/views.py
@@ -404,7 +404,9 @@ def deck_edit(request, deck_id: int) -> HttpResponse:
                     deck_card.full_clean()
                     deck_card.save()
 
-                if not deck_form.cleaned_data.get("skip_validation"):
+                if not deck_form.cleaned_data.get(
+                    "skip_validation"
+                ) and not deck_form.cleaned_data.get("is_prototype"):
                     deck.validate_format()
 
             if not request.POST.get("save_continue"):
