19a5244b1d78a0d18ef6b1753dd0789d6f1c16b2
marshl <marshl@live.com.au>
2022-09-11 23:28:08 +1000

    Change deck export command to be able to export word documents

--- sylvan_library/data_export/management/commands/deck_export.py
+++ sylvan_library/data_export/management/commands/deck_export.py
@@ -5,12 +5,15 @@ The module for the deck_export command
 import logging
 import os
 import re
+from typing import List
 
+import docx
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 from django.utils.text import slugify
 
-from cards.models.decks import Deck
+from cards.models.decks import Deck, DeckCard
+from cards.models.sets import Set
 from website.templatetags.deck_templatetags import deck_card_group_count
 
 logger = logging.getLogger("django")
@@ -51,16 +54,100 @@ class Command(BaseCommand):
 
         os.makedirs(output_directory, exist_ok=True)
 
-        self.export_decks(output_directory, user)
+        # self.export_decks(output_directory, user)
+        self.export_pretty_decks(output_directory, user)
+
+    def get_ordinal_suffix(self, n):
+        """
+        Convert an integer into its ordinal representation::
+
+            make_ordinal(0)   => '0th'
+            make_ordinal(3)   => '3rd'
+            make_ordinal(122) => '122nd'
+            make_ordinal(213) => '213th'
+        """
+        n = int(n)
+        if 11 <= (n % 100) <= 13:
+            suffix = "th"
+        else:
+            suffix = ["th", "st", "nd", "rd", "th"][min(n % 10, 4)]
+        return suffix
+
+    def export_pretty_decks(self, output_directory: str, user: get_user_model()):
+        deck = user.decks.order_by("-date_created").first()
+
+        template_document = docx.Document(
+            os.path.join("data_export", "templates", "edh_deck_template.docx")
+        )
+
+        deck_release_date_str = deck.date_created.strftime("%d{} of %B, %Y").format(
+            self.get_ordinal_suffix(deck.date_created.day)
+        )
+
+        latest_set = (
+            Set.objects.filter(
+                release_date__lte=deck.date_created,
+                type__in=("masters", "expansion", "core"),
+            )
+            .order_by("-release_date")
+            .first()
+        )
+
+        for paragraph in template_document.paragraphs:
+            if "<DECK_NAME>" in paragraph.text:
+                paragraph.text = paragraph.text.replace("<DECK_NAME>", deck.name)
+            if "<DECK_SUBTITLE>" in paragraph.text:
+                paragraph.text = paragraph.text.replace(
+                    "<DECK_SUBTITLE>", deck.subtitle
+                )
+            if "<DECK_RELEASE_DATE>" in paragraph.text:
+                paragraph.text = paragraph.text.replace(
+                    "<DECK_RELEASE_DATE>", deck_release_date_str
+                )
+            if "<DECK_LATEST_SET>" in paragraph.text:
+                paragraph.text = paragraph.text.replace(
+                    "<DECK_LATEST_SET>", latest_set.name
+                )
+
+        card_groups = {group["code"]: group for group in deck.get_card_groups()}
+
+        for table in template_document.tables:
+            header = table.rows[0]
+            is_basic_land = len(header.cells) == 2
+            if not is_basic_land:
+                header_text = header.cells[0].text
+                print(header_text)
+                if header_text.endswith("General"):
+                    card_group = card_groups["commander"]
+                elif header_text.endswith("Nonbasic Land"):
+                    card_group = card_groups["land"]
+                else:
+                    card_group = card_groups[header_text]
+
+                deck_cards: List[DeckCard] = card_group["cards"]
+                count = sum(deck_card.count for deck_card in deck_cards)
+                header.cells[0].paragraphs[0].text = f"{count} {card_group['name']}"
+
+                if deck_cards:
+                    first_row = table.rows[1].cells[0]
+                    first_row.paragraphs[0].text = deck_cards[0].card.name
+                    for deck_card in deck_cards[1:]:
+                        row = table.add_row()
+                        paragraph = row.cells[0].paragraphs[0]
+                        paragraph.style = first_row.paragraphs[0].style
+                        paragraph.text = deck_card.card.name
+
+        template_document.save(os.path.join(output_directory, "output.docx"))
+        pass
 
     def export_decks(self, output_directory: str, user: get_user_model()):
         deck: Deck
         for deck in user.decks.filter(is_prototype=False):
             deck_slug = slugify(f"{deck.date_created.isoformat()} {deck.name}")
             filename = os.path.join(output_directory, f"{deck_slug}.txt")
+            if os.path.exists(filename):
+                continue
             print(deck.name, filename)
-            # if os.path.exists(filename):
-            #     continue
 
             deck = Deck.objects.prefetch_related("cards__card").get(id=deck.id)
 
@@ -93,7 +180,9 @@ class Command(BaseCommand):
                 for card_group in deck.get_card_groups():
                     if not card_group["cards"]:
                         continue
-                    file.write(f"// {deck_card_group_count(card_group['cards'])} {card_group['name']}\n")
+                    file.write(
+                        f"// {deck_card_group_count(card_group['cards'])} {card_group['name']}\n"
+                    )
                     for card in card_group["cards"]:
                         file.write(f"{card.as_deck_text()}\n")
 
