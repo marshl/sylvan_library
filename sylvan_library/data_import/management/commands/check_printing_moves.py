d33c5fef1640318e3999dff25a41346a606d84c6
marshl <marshl@live.com.au>
2023-03-19 22:54:51 +1100

    Add a script to check for cards being moved from one set to another

---
+++ sylvan_library/data_import/management/commands/check_printing_moves.py
@@ -0,0 +1,101 @@
+"""
+Module for the update_database command
+"""
+import logging
+import time
+import typing
+from typing import List, Optional, Tuple
+
+from django.core.management.base import BaseCommand
+from django.db import transaction, models
+from django.db.models import F
+from django.urls import reverse
+
+from cards.models.card import (
+    CardFace,
+    CardFacePrinting,
+    CardLocalisation,
+    CardFaceLocalisation,
+    Card,
+    CardPrinting,
+)
+from cards.models.sets import Set, Block
+from data_import.management.commands import get_all_set_data
+from data_import.models import (
+    UpdateSet,
+    UpdateCard,
+    UpdateBlock,
+    UpdateMode,
+    UpdateCardFace,
+    UpdateCardPrinting,
+    UpdateCardFacePrinting,
+    UpdateCardLocalisation,
+    UpdateCardFaceLocalisation,
+    UpdateCardRuling,
+    UpdateCardLegality,
+)
+from data_import.staging import (
+    StagedCard,
+    StagedSet,
+    StagedCardLocalisation,
+    StagedCardPrinting,
+    StagedCardFace,
+    StagedCardFacePrinting,
+    StagedCardFaceLocalisation,
+)
+
+logger = logging.getLogger("django")
+
+
+def get_admin_link(scryfall_id):
+    obj = CardPrinting.objects.get(scryfall_id=scryfall_id)
+    return "http://localhost:8000" + reverse(
+        "admin:cards_cardprinting_change", args=(obj.id,)
+    )
+
+
+class Command(BaseCommand):
+    """
+    The command for updating hte database
+    """
+
+    help = (
+        "Uses the downloaded JSON files to update the database, "
+        "including creating cards, set and rarities\n"
+    )
+
+    def __init__(self, stdout=None, stderr=None, no_color=False):
+        super().__init__(stdout=stdout, stderr=stderr, no_color=no_color)
+
+    def handle(self, *args, **options):
+        existing_scryfall_ids = {
+            c[0]: c[1]
+            for c in CardPrinting.objects.values_list("scryfall_id", "set__code")
+        }
+        for set_data in get_all_set_data(options.get("set_codes")):
+            set_code = set_data["code"]
+            for card in set_data["cards"]:
+                scryfall_id = card["identifiers"]["scryfallId"]
+                if scryfall_id not in existing_scryfall_ids:
+                    existing_scryfall_ids[scryfall_id] = set_code
+                    continue
+
+                if existing_scryfall_ids[scryfall_id] != set_code:
+                    logger.info(
+                        "Printing %s (%s) in %s is also in %s %s",
+                        card["name"],
+                        scryfall_id,
+                        existing_scryfall_ids[scryfall_id],
+                        set_code,
+                        get_admin_link(scryfall_id),
+                    )
+        for set_data in get_all_set_data(options.get("set_codes")):
+            for card in set_data["cards"]:
+                scryfall_id = card["identifiers"]["scryfallId"]
+                try:
+                    existing_scryfall_ids.pop(scryfall_id)
+                except KeyError:
+                    pass
+
+        for scryfall_id, set_code in existing_scryfall_ids.items():
+            logger.info("Leftover scryfall ID %s in set %s", scryfall_id, set_code)
